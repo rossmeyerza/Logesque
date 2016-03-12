25371dc044223707b98084585eec023ef1780bfa
Ross Meyer <ross@Rosss-MacBook.local>
2016-03-12 08:46:32 +0200

    Sockets specific javascript

--- public/javascripts/sockets.js
+++ public/javascripts/sockets.js
@@ -1,5 +1,23 @@
+function draw() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect (10, 10, 55, 50);
+
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect (30, 30, 55, 50);
+    }
+}
+
+
 var socket = io();
 
+var div = document.getElementById('log');
+
 socket.on('log', function(msg) {
-	 $('#log').append($('<li>').text(msg));
+    div.innerHTML += '<li>' + msg + '</li>';
 })
+
+window.onload = "draw()";
